41890b7561d2b8b247b114839f646ca5e40ddac7
Kurt Adrian G. Luzoraga <kurtluzoraga24@gmail.com>
2026-02-02 15:08:05 +0800

    Fixed nav bar and better ui

--- app/src/main/java/com/example/kitago/ProfileActivity.kt
+++ app/src/main/java/com/example/kitago/ProfileActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
-class DashboardActivity : ComponentActivity() {
+class ProfileActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_profile)
 
         setupNavigation()
     }
 
     private fun setupNavigation() {
+        findViewById<ImageButton>(R.id.navHome).setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
         findViewById<ImageButton>(R.id.navGoals).setOnClickListener {
             startActivity(Intent(this, GoalsActivity::class.java))
         }
@@ -22,9 +25,6 @@ class DashboardActivity : ComponentActivity() {
         }
         findViewById<ImageButton>(R.id.navChallenges).setOnClickListener {
             startActivity(Intent(this, ChallengesActivity::class.java))
-        }
-        findViewById<ImageButton>(R.id.navProfile).setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
         }
     }
 }
